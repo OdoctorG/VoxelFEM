90a75da1ddeb3beb51fdf962502ed5bd2837a8c6
Oscar Gren <grenoscar@gmail.com>
2024-11-14 00:00:56 -0800

    Plotting eff. improvments

--- femplotter.py
+++ femplotter.py
@@ -1,9 +1,13 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import matplotlib.cm as cm
 from matplotlib.colors import Normalize
 from femsolver import nodes_to_coord, coord_to_nodes, shape_function
+
+import matplotlib.style as mplstyle
+mplstyle.use('fast')
 
 def plot_mesh(voxels: np.ndarray, new_figure: bool = False, offset: np.ndarray = np.array([0, 0]),
             flip_y: bool = False, z_order=2) -> plt.Figure:
@@ -114,26 +118,26 @@ def node_vector_plot(vals: np.ndarray, voxels: np.ndarray, scale=10e9) -> plt.Fi
 
     Returns
     -------
-    None
+    plt.Figure
+        matplotlib figure
     """
     
     fig = plt.figure()
     
-    print(vals.shape)
     norm = Normalize(vals.min(), vals.max())
     for i in range(len(vals)//2):
         coord = nodes_to_coord(i, voxels.shape[1])
 
-        vector_length = np.sqrt(vals[i*2][0]**2 + vals[i*2+1][0]**2)
+        vector_length = np.sqrt(vals[i*2]**2 + vals[i*2+1]**2)
         color = cm.viridis(norm(vector_length))
 
         if vector_length > 0:
-            plt.arrow(coord[1], coord[0], scale*vals[i*2][0], scale*vals[i*2+1][0], color=color, head_width=np.log(vector_length*scale*0.5+1)+0.01,
+            plt.arrow(coord[1], coord[0], scale*vals[i*2], scale*vals[i*2+1], color=color, head_width=np.log(vector_length*scale*0.5+1)+0.01,
                     zorder=3)
     fig.axes[0].invert_yaxis()
     plot_mesh(voxels)
     return fig
-def node_value_plot(vals: np.ndarray, voxels: np.ndarray, scale=1) -> plt.Figure:
+def node_value_plot(vals: np.ndarray, voxels: np.ndarray) -> plt.Figure:
     """
     Plot a 2D interpolation of the given values at each node in the mesh, given the voxel representation of the geometry.
 
@@ -143,26 +147,25 @@ def node_value_plot(vals: np.ndarray, voxels: np.ndarray, scale=1) -> plt.Figure
         1D array of values at each node
     voxels : np.ndarray
         2D array of voxels, where 1 indicates a solid voxel and 0 indicates a void voxel.
-    scale : float
-        Scale factor for the plot.
 
     Returns
     -------
-    None
+    plt.Figure
+        matplotlib figure
     """
 
     fig = plt.figure()
     norm = plt.Normalize(0.9*min(vals), max(vals)*4)
     # Vectorize the function
     vectorized_compute_vector = np.vectorize(shape_function, signature='(),()->(n)')
-    print(vals.shape)
+
     for i in range(voxels.shape[0]):
         for j in range(voxels.shape[1]):
             if (voxels[i, j] == 1):
                 coord = coord_to_nodes(i, j, voxels.shape[1])
                 z = [vals[coord[0]], vals[coord[1]], vals[coord[2]], vals[coord[3]]]
-                x = np.linspace(-1, 1, 100)
-                y = np.linspace(-1, 1, 100)
+                x = np.linspace(-1, 1, 10)
+                y = np.linspace(-1, 1, 10)
                 X, Y = np.meshgrid(x, y)
 
 
@@ -173,7 +176,7 @@ def node_value_plot(vals: np.ndarray, voxels: np.ndarray, scale=1) -> plt.Figure
                 result = np.dot(vectors, z)
 
                 # Plot the result
-                plt.pcolormesh(X*0.5+0.5+j, Y*(0.5)+0.5+i, result, cmap='viridis', norm=norm)
+                plt.pcolormesh(X*0.5+0.5+j, Y*(0.5)+0.5+i, result, cmap='viridis', norm=norm, shading='gouraud')
     plt.colorbar()
     fig.axes[0].invert_yaxis()
     plot_mesh(voxels)
